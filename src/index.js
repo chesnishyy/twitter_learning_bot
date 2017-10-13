39b936eea6358c038fa49bb950529e157d9a1837
Igor C. | Dreamscape <Igor.C@dreamscapenetworks.com>
2017-10-13 15:47:53 +0300

    Added friendships/lookup and send direct message  functions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv'
 import Twit from 'twit';
+import fs from 'fs';
 
 dotenv.config();
 
@@ -14,21 +15,61 @@ const bot = new Twit({
 
 	// post a tweet
 
-bot.post('statuses/update', {status: 'Hello, World from my learning twitter bot!'}, (error, data, response) => {
-	if (error) {
-		console.error(error)
-	} else {
-		console.log(data.text + 'was tweeted.');
-	}
-});
+// bot.post('statuses/update', {status: 'Hello, World from my learning twitter bot!'}, (error, data, response) => {
+// 	if (error) {
+// 		console.error(error)
+// 	} else {
+// 		console.log(data.text + 'was tweeted.');
+// 	}
+// });
 
 
 	// log to console followers ids
 
-bot.get('followers/ids', {screen_name: 'igor_pinochet'}, (error, data, response) => {
+// bot.get('followers/ids', {screen_name: 'igor_pinochet'}, (error, data, response) => {
+// 	if (error) {
+// 		console.log(error)
+// 	} else {
+// 		console.log(data)
+// 	}
+// });
+
+
+// log to console list of followers
+
+
+// bot.get('followers/list', {screen_name: 'IamKrus'}, (error, data) => {
+// 	if (error) {
+// 		console.log(error)
+// 	} else {
+// 		data.users.map(user => console.log(user.screen_name))
+//
+// 	}
+// });
+
+
+// bot.get('friends/list', {screen_name: 'igor_pinochet'}, (error, data) => {
+// 	if (error) {
+// 		console.log(error)
+// 	} else {
+// 		console.log(data);
+// 	}
+// });
+
+bot.get('friendships/lookup', {screen_name: 'wowMariya'}, (error, data) => {
 	if (error) {
 		console.log(error)
 	} else {
-		console.log(data)
+		console.log(data);
+	}
+});
+
+// send new direct message
+
+bot.post('direct_messages/new', {screen_name: 'wowMariya', text: 'Hello Masha'}, (error, data) => {
+	if (error) {
+		console.log(error)
+	} else {
+		console.log(data);
 	}
 });
